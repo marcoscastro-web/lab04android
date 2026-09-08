4ae34aa25faf5a3f29140b03c11d6f09b00978dd
marcoscastro-web <marcos.castro@tecsup.edu.pe>
2026-09-08 09:48:19 -0500

    feat: Add TextField component

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -6,13 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab04.ui.theme.Lab04Theme
-import androidx.compose.material3.Button
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,4 +57,10 @@ fun MiBotonEjemplo() {
     Button(onClick = { }) {
         Text("Mi Botón")
     }
+}
+
+@Composable
+fun MiCampoTextoEjemplo() {
+    var texto by remember { mutableStateOf("") }
+    TextField(value = texto, onValueChange = { texto = it }, label = { Text("Nombre") })
 }
