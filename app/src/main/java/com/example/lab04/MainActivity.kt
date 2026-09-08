c787f41f4ed0cb2b0152795d10bef26a38ca661f
marcoscastro-web <marcos.castro@tecsup.edu.pe>
2026-09-08 09:55:22 -0500

    Revert "feat: Add ViewHolaCurso"
    This reverts commit 4d8e68df2278bf6bccfd4ddfb48fb782756daa3b.

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -64,22 +64,3 @@ fun MiCampoTextoEjemplo() {
     var texto by remember { mutableStateOf("") }
     TextField(value = texto, onValueChange = { texto = it }, label = { Text("Nombre") })
 }
-
-fun ViewHolaCurso() {
-    Column(
-        modifier = Modifier
-            .fillMaxWith()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Welcome to the Course!",
-            fontSize = 28.sp,
-            fontWeight = FontWeigh.Bold
-        )
-        Spacer(modifier = Modifier.heigh(16.dp))
-        Text(
-            text = "Hello, Student!",
-            fontSize = 20.xD
-    }
-}
